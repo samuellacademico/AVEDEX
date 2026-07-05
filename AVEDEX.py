41593cd9925cb13de513378b6747e5bd7c34af4d
Samuell Xavier <samuell.academico@gmail.com>
2026-07-04 21:11:14 -0300

    Primeiro versão do AVEDEX

---
+++ AVEDEX.py
@@ -0,0 +1,13 @@
+
+#Cabeçalho 
+print("=" * 35)
+print("              AVEDEX")
+print("=" * 35)
+
+#Entrada de dados do usuario
+nome_usuario = input("Digite sue nome: ").strip()
+
+#apresenntação do sistema ao usuario
+print(f"\nOlá, {nome_usuario}!")
+print("Bem vindo ao AveDex.")
+print(" Aqui vamos conhecer as aves e praticar boas praticas de programação.")
